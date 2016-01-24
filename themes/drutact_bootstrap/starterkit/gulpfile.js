ac3491120606fb35612051aa61379ea9f81892c9
Pascal Martineau <pascal.animateur@gmail.com>
2016-01-24 10:45:44 -0500

    Initial DruTACT Bootstrap starterkit.

---
+++ themes/drutact_bootstrap/starterkit/gulpfile.js
@@ -0,0 +1,20 @@
+var gulp       = require('gulp');
+var gutil      = require('gulp-util');
+var less       = require('gulp-less');
+var watch      = require('gulp-watch');
+var livereload = require('gulp-livereload');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('less', function () {
+    gulp.src('less/style.less')
+        .pipe(sourcemaps.init())
+        .pipe(less().on('error', gutil.log))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('css'))
+        .pipe(livereload());
+});
+
+gulp.task('watch', function() {
+    livereload.listen();
+    gulp.watch(['less/*.less', '../../../../profiles/drutact/themes/drutact_bootstrap/less/*.less'], ['less']);
+});
