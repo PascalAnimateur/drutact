62a7716e4c9c0c1034b4c00ce33411cabe57013f
Pascal Martineau <pascal.animateur@gmail.com>
2016-01-24 10:29:56 -0500

    Initial templates.

---
+++ themes/drutact_bootstrap/templates/field--og-group-ref.tpl.php
@@ -0,0 +1,10 @@
+<div class="<?php print $classes; ?>"<?php print $attributes; ?>>
+  <?php if (!$label_hidden): ?>
+    <div class="field-label" <?php print $title_attributes; ?>><i class="fa fa-users" title="<?php print t('Groups audience'); ?>"></i>&nbsp;</div>
+  <?php endif; ?>
+  <div class="field-items" <?php print $content_attributes; ?>>
+    <?php foreach ($items as $delta => $item): ?>
+      <div class="field-item label label-primary" <?php print $item_attributes[$delta]; ?>><?php print render($item); ?></div>
+    <?php endforeach; ?>
+  </div>
+</div>
