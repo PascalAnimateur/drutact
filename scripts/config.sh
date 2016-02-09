ab8f3ea7795a580dd7f88251ebd3d09ad0c5e175
Pascal Martineau <pascal.animateur@gmail.com>
2016-02-09 10:28:27 -0500

    Extract translations script.

---
+++ scripts/config.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+##### INCLUSION GUARD #####
+if [ ! -n "$DRUTACT_VERSION" ] ; then
+  export DRUTACT_VERSION="7.x-1.3-alpha25"
+  export SCRIPTS_PATH=`readlink -f $(dirname $0)`
+  export DRUTACT_PATH=`readlink -f $(dirname $0)/..`
+  export DRUPAL_ROOT=`readlink -f $(dirname $0)/../../..`
+  echo -e "\e[36m### DruTACT $DRUTACT_VERSION ###\e[0m"
+  echo ""
+else
+  return
+fi
+
+##### LOG FILE #####
+LOG_FILE="$SCRIPTS_PATH/log/`basename $0 .sh`.log"
+touch $LOG_FILE
+
+##### MESSAGE / CONFIRM / WARNING / ERROR #####
+export MESSAGE_INDENT=""
+function INDENT () {
+  MESSAGE_INDENT="$MESSAGE_INDENT  "
+}
+function OUTDENT () {
+  MESSAGE_INDENT=${MESSAGE_INDENT::-2}
+}
+function MESSAGE () {
+  echo -e "[`basename $0`] ${MESSAGE_INDENT}$1"
+}
+function CONFIRM () {
+  echo -e "[`basename $0`] ${MESSAGE_INDENT}\e[93mINFO:\e[0m $1"
+  if [ "$YES" != "yes" ] ; then
+    read -p "[`basename $0`] ${MESSAGE_INDENT}Do you want to continue [y/N]? " -n 1 -r
+    if [[ ! $REPLY =~ ^[Yy]$ ]] ; then echo " " ; echo -e " " ; exit 0 ; fi
+    echo " "
+    #echo " "
+  fi
+}
+function WARNING () {
+  echo -e "[`basename $0`] ${MESSAGE_INDENT}\e[93mWARNING:\e[0m $1"
+  if [ "$YES" != "yes" ] ; then
+    read -p "[`basename $0`] ${MESSAGE_INDENT}Do you want to continue [y/N]? " -n 1 -r
+    if [[ ! $REPLY =~ ^[Yy]$ ]] ; then echo " " ; echo -e " " ; exit 0 ; fi
+    echo " "
+  fi
+}
+function ERROR () {
+  echo -e "[`basename $0`] ${MESSAGE_INDENT}\e[91mERROR:\e[0m $1"
+  exit 1
+}
---
+++ scripts/extract-translations.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+##### CONFIGURATION #####
+if [ -e $(dirname $0)/config.sh ] ; then
+  . $(dirname $0)/config.sh
+else
+  echo -e "[`basename $0`] ${MESSAGE_INDENT}\e[91mERROR:\e[0m DruTACT configuration script not found: \e[36m$(dirname $0)/config.sh\e[0m"
+  exit 1
+fi
+
+##### EXTRACT TRANSLATIONS #####
+cd $DRUPAL_ROOT
+for site in sites/* ; do
+  if [ -e $site/settings.php ] ; then
+    cd $site
+    MESSAGE "Extracting translations in $site..."
+    INDENT
+
+    MESSAGE "Switching to Development mode..."
+    drush tadaa-switch dev >> $LOG_FILE 2>&1
+
+    MESSAGE "Moving contrib libraries / modules / themes out of DruTACT profile..."
+    mv ../../profiles/drutact/libraries ../all/libraries
+    mv ../../profiles/drutact/modules/contrib ../all/modules
+    mv ../../profiles/drutact/themes/contrib ../all/themes
+    drush rr >> $LOG_FILE 2>&1
+    drush cc drush >> $LOG_FILE 2>&1
+
+    MESSAGE "Localizing fields..."
+    drush localize-fields ../../profiles/drutact -y >> $LOG_FILE 2>&1
+    drush localize-fields ../../profiles/drutact -y >> $LOG_FILE 2>&1
+
+    MESSAGE "Extracting translations..."
+    drush potx single --translations --language=fr --folder=profiles/drutact >> $LOG_FILE 2>&1
+
+    MESSAGE "Moving contrib libraries / modules / themes back into DruTACT profile..."
+    mv ../all/libraries ../../profiles/drutact/libraries
+    mv ../all/modules/contrib ../../profiles/drutact/modules
+    mv ../all/themes/contrib ../../profiles/drutact/themes
+    drush rr >> $LOG_FILE 2>&1
+    drush cc drush >> $LOG_FILE 2>&1
+
+    MESSAGE "Moving translations into DruTACT profile..."
+    mv ../../general.pot ../../profiles/drutact/translations/drutact.fr.po
+
+    OUTDENT
+  fi
+done
