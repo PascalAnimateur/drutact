1fa0aa805de1eead976b9b5f2dec7dbd317937ba
Pascal Martineau <pascal.animateur@gmail.com>
2016-02-16 18:42:33 -0500

    Newsletter item templates for drutact_bartik and drutact_bootstrap.

--- themes/drutact_bartik/templates/node--newsletter-item.tpl.php
+++ themes/drutact_bartik/templates/node--newsletter-item.tpl.php
@@ -3,15 +3,13 @@
  * Node template for the Newsletter item view mode.
  */
 ?>
-<div class="media">
-  <div class="media-left">
-    <?php if (isset($content['field_image'])): ?>
+<div class="clearfix">
+  <?php if (isset($content['field_image'])): ?>
+    <div class="newsletter-item-image" style="float: left; margin: 10px;">
       <?php print render($content['field_image']) ?>
-    <?php endif; ?>
-  </div>
-  <div class="media-body">
-    <h4 class="media-heading"><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h4>
-    <?php print render($content['body']) ?>
-    <?php print render($content['links']) ?>
-  </div>
+    </div>
+  <?php endif; ?>
+  <h2 class="newsletter-item-title"><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+  <?php print render($content['body']) ?>
+  <?php print render($content['links']) ?>
 </div>
--- themes/drutact_bootstrap/templates/node--newsletter-item.tpl.php
+++ themes/drutact_bootstrap/templates/node--newsletter-item.tpl.php
@@ -3,14 +3,15 @@
  * Node template for the Newsletter item view mode.
  */
 ?>
-
-<div class="clearfix">
-  <?php if (isset($content['field_image'])): ?>
-    <div class="newsletter-item-image" style="float: left; margin: 10px;">
+<div class="media">
+  <div class="media-left">
+    <?php if (isset($content['field_image'])): ?>
       <?php print render($content['field_image']) ?>
-    </div>
-  <?php endif; ?>
-  <h2 class="newsletter-item-title"><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-  <?php print render($content['body']) ?>
-  <?php print render($content['links']) ?>
+    <?php endif; ?>
+  </div>
+  <div class="media-body">
+    <h4 class="media-heading"><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h4>
+    <?php print render($content['body']) ?>
+    <?php print render($content['links']) ?>
+  </div>
 </div>
