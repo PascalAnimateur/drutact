29d60f2376cff10d29dc35638beb3aa622e5fecf
Pascal Martineau <pascal.animateur@gmail.com>
2016-02-20 22:16:11 -0500

    Email styling generated from LESS files in DruTACT Bootstrap starterkit.

---
+++ themes/drutact_bootstrap/starterkit/templates/mimemail-message.tpl.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @file
+ * DruTACT theme implementation to format an HTML mail.
+ *
+ * Available variables:
+ * - $recipient: The recipient of the message
+ * - $subject: The message subject
+ * - $body: The message body
+ * - $css: Internal style sheets
+ * - $module: The sending module
+ * - $key: The message identifier
+ * - $base_url: The base URL of the site
+ * - $logo_url: The base URL of the site logo
+ *
+ * @see template_preprocess_mimemail_message()
+ */
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <?php if ($css): ?>
+    <style type="text/css">
+      <!--
+      <?php print $css ?>
+      -->
+    </style>
+  <?php endif; ?>
+  </head>
+  <body id="mimemail-body" <?php if ($module && $key): print 'class="'. $module .'-'. $key .'"'; endif; ?>>
+    <div id="center">
+      <div id="logo">
+        <a href="<?php print $base_url ?>"><img src="<?php print $logo_url ?>" /></a>
+      </div>
+      <div id="main">
+        <?php print $body ?>
+      </div>
+    </div>
+  </body>
+</html>
